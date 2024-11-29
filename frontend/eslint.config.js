b2335cb8da2c9d9305813684cf689739923c8582
SV <vshree852@gmail.com>
2024-11-28 20:37:44 -0800

    fix eslint

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,14 +1,20 @@
+const tsParser = require("@typescript-eslint/parser");
 module.exports = [
   {
-    files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"], // Specify file types
+    files: ["**/*.ts", "**/*.tsx", "**/*.js", "**/*.jsx"], // Specify file types
     languageOptions: {
       ecmaVersion: "latest", // Latest JavaScript version
-      sourceType: "module",  // Support for ES modules
+      sourceType: "module", // Support for ES modules
+      parser: tsParser, // Ensure parser is set
       parserOptions: {
         ecmaFeatures: {
           jsx: true, // Enable JSX parsing for React projects
         },
       },
+    },
+    plugins: {
+      "@typescript-eslint": require("@typescript-eslint/eslint-plugin"),
+      prettier: require("eslint-plugin-prettier"),
     },
     rules: {
       "no-unused-vars": "warn",
