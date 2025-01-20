5c160c3827406cfa1a45acabe4bc995d5b55511a
Chris <comosjay@gmail.com>
2025-01-19 20:37:30 -0800

    prettier

--- frontend/components/TransactionHistory/FullTransactionHistory.tsx
+++ frontend/components/TransactionHistory/FullTransactionHistory.tsx
@@ -4,25 +4,25 @@ import { FontAwesome } from "@expo/vector-icons";
 
 //container for the recent transaction history.
 export default function FullTransactionHistory(props: any) {
-    //Constant array that groups the props list by their dates
-    const groupedByDate = props.list.reduce((acc: any, transaction: any) => {
-        //if the date does not exist, add a new hashmap for it
-        if (!acc[transaction.date]) {
-        acc[transaction.date] = [];
-        }
-        //if it exists, push new element to the date
-        acc[transaction.date].push(transaction);
-        return acc;
-    },{});
+  //Constant array that groups the props list by their dates
+  const groupedByDate = props.list.reduce((acc: any, transaction: any) => {
+    //if the date does not exist, add a new hashmap for it
+    if (!acc[transaction.date]) {
+      acc[transaction.date] = [];
+    }
+    //if it exists, push new element to the date
+    acc[transaction.date].push(transaction);
+    return acc;
+  }, {});
   return (
     <View style={styles.HistoryContainer}>
-      <Text style={{ fontSize: 20, fontWeight: '500' }}>Transactions</Text>
+      <Text style={{ fontSize: 20, fontWeight: "500" }}>Transactions</Text>
       {/* Scrollview for scrolling */}
       <ScrollView>
         <View style={styles.recentTranactions}>
-        {/* mapping through the different date categories */}
+          {/* mapping through the different date categories */}
           {Object.entries(groupedByDate).map(([date, transactions]: any) => (
-            <View style={styles.row}  key = {date}>
+            <View style={styles.row} key={date}>
               <Text style={styles.dates}>{date}</Text>
               {/* for Each date category, we map the elements(transactions) that are associated with that date */}
               {transactions.map(
@@ -30,12 +30,12 @@ export default function FullTransactionHistory(props: any) {
                   transaction: { name: any; date: any; amount: any; id: any },
                   index: any,
                 ) => (
-                  <View key = {transaction.id}>
+                  <View key={transaction.id}>
                     <TransactionRow
                       name={transaction.name}
                       date={transaction.date}
                       amount={transaction.amount}
-                    //   key={transaction.id}
+                      //   key={transaction.id}
                     />
                     {/* Put a separater between elements excpet for the last one */}
                     {index < transactions.length - 1 && (
@@ -77,7 +77,7 @@ const styles = StyleSheet.create({
   },
   dates: {
     fontSize: 18,
-    fontWeight: '500',
+    fontWeight: "500",
     opacity: 0.4,
   },
   separator: {
