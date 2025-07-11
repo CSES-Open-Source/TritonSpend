cecd51dd9833a648a4af5028dcbfc0a561039125
Vedant Vardhaan <vedantvardhaan@Vedants-MacBook-Air-M4.local>
2025-07-11 23:24:18 +0530

    setup

--- backend/src/db/db.ts
+++ backend/src/db/db.ts
@@ -10,10 +10,7 @@ const client = new Client({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  ssl: {
-    rejectUnauthorized: false, // okay for development, not production
-  },
-});
+  ssl: false});
 
 // Connect to PostgreSQL
 client
